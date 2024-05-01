e388603ab3da59ab9a131ac559ca2329a0767ab8
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-01 15:01:27 +0300

    random dog display working

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React,{useEffect, useState} from "react";
+function App (){
+    const [loaded,setLoaded]=useState(false)
+    const [image,setImage]=useState(null)
+    const url = "https://dog.ceo/api/breeds/image/random"
+    useEffect(()=>{
+        fetch(url)
+        .then(r=>r.json())
+        .then(data=>{setImage(data.message)
+         setLoaded(true)
+        }).catch(err=>console.error(`error fetching random dog pic ${err}`))
+    },[])
+    return (
+        <>
+        <h1>Random Dog Image</h1>
+        {loaded ? <img alt="A Random Dog" src={image}/>:<p>Loading...</p>}
+        
+        </>
+    )
+}
+export default App
